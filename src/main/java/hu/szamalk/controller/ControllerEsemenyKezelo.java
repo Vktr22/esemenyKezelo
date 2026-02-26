7ea32f3cc416dd6a130005cefe2c37ee0cef8ea4
Vktr22 <nds.viktoria@gmail.com>
2026-02-26 15:31:29 +0100

    esemenymodell+alapfileok

---
+++ src/main/java/hu/szamalk/controller/ControllerEsemenyKezelo.java
@@ -0,0 +1,4 @@
+package hu.szamalk.controller;
+
+public class ControllerEsemenyKezelo {
+}
---
+++ src/main/java/hu/szamalk/modell/EsemenyModell.java
@@ -0,0 +1,23 @@
+package hu.szamalk.modell;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class EsemenyModell {
+    private String esemenyAzon;
+    private String kapcsolatTarto;
+    private LocalDate datum;
+    private LocalTime kezdesOraPerc;
+    private  LocalTime vegeOraPerc;
+    private int teremSzama;
+    private int resztvevokSzama;
+    private String statusz;
+    private boolean tipus;
+    private boolean szekKellE;
+    private int szekekSzama;
+    private boolean asztalKellE;
+    private int asztalokSzama;
+
+
+
+}
